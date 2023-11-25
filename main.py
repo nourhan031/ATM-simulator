55b7ca321fdaff673e59e1a48fdb588ed39eb582
ShadenAli95 <102587160+ShadenAli95@users.noreply.github.com>
2023-11-25 21:40:02 +0200

    last update

---
+++ main.py
@@ -0,0 +1,103 @@
+import random
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+
+num_customers = 10
+Customers = []
+InterArrivalTime = []
+ArrivalTime = []
+StartTime = []
+WaitingTime = []
+ServiceTime = []
+CompletionTime = []
+TimeInSystem = []
+ATM1 = []
+ATM2 = []
+
+def generate_IAT():
+    return np.random.uniform(0,5)
+
+def generate_ST():
+    return np.random.normal(2, 0.5)
+
+#(Normal distribution with mean=2, std=0.5)
+ServiceTime1 = []
+ServiceTime2 = []
+in1=0
+in2=0
+
+
+
+for i in range(num_customers):
+    Customers.append(i+1)
+    if i == 0: #Customer 1
+        IAT = generate_IAT()
+        IAT = math.floor(IAT * 10) / 10
+        InterArrivalTime.append(IAT)
+        ArrivalTime.append(IAT)
+        StartTime.append(ArrivalTime[i])
+        WaitingTime.append(0)
+        ST = generate_ST()
+        ST = math.floor(ST * 10) / 10
+        ServiceTime.append(ST)
+        ServiceTime1.append(ST)
+        in1 = in1+1
+        CompletionTime.append(StartTime[i]+ ServiceTime1[i])
+        TimeInSystem.append(CompletionTime[i] - ArrivalTime[i])
+        ATM1.append(CompletionTime[i])
+        ATM2.append(0)
+    else:
+        IAT = generate_IAT()
+        IAT = math.floor(IAT * 10) / 10
+        InterArrivalTime.append(IAT)
+        ArrivalTime.append(ArrivalTime[i - 1] + InterArrivalTime[i])
+        atm = min(ATM1[i-1],ATM2[i-1])
+        StartTime.append(max(ArrivalTime[i],atm))
+        WaitingTime.append(StartTime[i]-ArrivalTime[i])
+        ST = generate_ST()
+        ST = math.floor(ST * 10) / 10
+        ServiceTime.append(ST)
+        if atm==ATM1[i-1]:
+            ServiceTime1.append(ST)
+            CompletionTime.append(StartTime[i] + ServiceTime1[in1])
+            in1 = in1+1
+            ATM1.append(CompletionTime[i])
+            ATM2.append(ATM2[i-1])
+        else:
+            ServiceTime2.append(ST)
+            CompletionTime.append(StartTime[i] + ServiceTime2[in2])
+            in2 =in2+1
+            ATM2.append(CompletionTime[i])
+            ATM1.append(ATM1[i-1])
+
+        TimeInSystem.append(CompletionTime[i] - ArrivalTime[i])
+
+
+AvgWaitingTime= sum(WaitingTime)/num_customers
+NumWaitingCust = sum(1 for wt in WaitingTime if wt > 0)
+TotalTime = max(CompletionTime)
+TotalSimTime = CompletionTime[num_customers-1]
+ProbOfWaiting = NumWaitingCust/num_customers
+UtilizationOfATM1 = sum(ServiceTime1)/ATM1[num_customers-1]
+UtilizationOfATM2 = sum(ServiceTime2)/ATM2[num_customers-1]
+AverageTIS = sum(TimeInSystem)/num_customers
+print("Average waiting time: ",AvgWaitingTime)
+print("Number of customers who had to wait: ",NumWaitingCust)
+print("The total time of the simulation: ",TotalSimTime)
+print("The probability that a customer will have to wait: ",ProbOfWaiting)
+print("Utilization of ATM1: ",UtilizationOfATM1)
+print("Utilization of ATM2: ",UtilizationOfATM2)
+print("Maximum number in the queue: ")
+print("Average time in the system: ",AverageTIS)
+print()
+#histogram for waiting time
+
+#print table
+num_elements = 15
+headers = ["Customer","InterArrival Time", "Arrival Time", "Service Start Time", "Waiting Time", "Service Time", "Completion Time", "Time In System", "ATM1", "ATM2"]
+data = list(zip(Customers[:num_elements],InterArrivalTime[:num_elements], ArrivalTime[:num_elements], StartTime[:15], WaitingTime[:num_elements],
+                ServiceTime[:num_elements], CompletionTime[:num_elements], TimeInSystem[:num_elements], ATM1[:num_elements], ATM2[:num_elements]))
+table = tabulate(data, headers, tablefmt="fancy_grid")
+print(table)
